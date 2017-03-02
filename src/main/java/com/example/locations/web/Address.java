d877fb920fc76aea85908021b8a5547d84b73119
Rafael Ordoñez <rafaelordonezvivar@gmail.com>
2017-03-02 00:18:01 +0000

    Created street and buildingName fields to returning addresses

--- src/main/java/com/example/locations/web/Address.java
+++ src/main/java/com/example/locations/web/Address.java
@@ -1,7 +1,32 @@
 package com.example.locations.web;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 /**
  * Created by rafa on 01/03/2017.
  */
 public class Address {
+    private final String street;
+    private final String buildingName;
+
+    @JsonCreator
+    public Address(@JsonProperty("street") String street, @JsonProperty("buildingName") String buildingName) {
+        this.street = street;
+        this.buildingName = buildingName;
+    }
+
+    @JsonCreator
+    public Address(@JsonProperty("street") String street) {
+        this.street = street;
+        this.buildingName = null;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
 }
--- src/test/java/com/example/locations/web/PostcodesControllerTest.java
+++ src/test/java/com/example/locations/web/PostcodesControllerTest.java
@@ -13,7 +13,11 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -52,17 +56,6 @@ public class PostcodesControllerTest {
     }
 
     @Test
-    public void getPostcodeShouldReturnAListOfAddresses() throws Exception {
-        String aPostcode = "SOME";
-
-        ResultActions response = this.mvc.perform(get("/postcodes/{postcode}", aPostcode)
-                .accept(MediaType.APPLICATION_JSON_UTF8));
-
-        response.andExpect(status().isOk())
-                .andExpect(jsonPath("$.addressList").exists());
-    }
-
-    @Test
     public void getPostcodeShouldCallAThirdPartyApiToSearchByPostcode() throws Exception {
         String aPostcode = "SOME";
         given(postcodeApi.getAddressListBy(aPostcode))
@@ -73,5 +66,25 @@ public class PostcodesControllerTest {
 
         response.andExpect(status().isOk());
         Mockito.verify(postcodeApi).getAddressListBy(aPostcode);
+    }
+
+    @Test
+    public void getPostcodeShouldReturnAListOfAddressWithStreetAndName() throws Exception {
+        List<Address> addressList = mockListOfAddresses();
+        String aPostcode = "SOME";
+        given(postcodeApi.getAddressListBy(aPostcode))
+                .willReturn(addressList);
+
+        ResultActions response = this.mvc.perform(get("/postcodes/{postcode}", aPostcode)
+                .accept(MediaType.APPLICATION_JSON_UTF8));
+
+        response.andExpect(status().isOk())
+                .andExpect(jsonPath("$.addressList", hasSize(addressList.size())))
+                .andExpect(jsonPath("$.addressList[*].street", hasSize(greaterThan(0))))
+                .andExpect(jsonPath("$.addressList[*].buildingName", hasSize(greaterThan(0))));
+    }
+
+    private List<Address> mockListOfAddresses() {
+        return Arrays.asList(new Address("Street 1", "Building name 1"), new Address("Street 2"), new Address("Street 3", "Building name 3"));
     }
 }
