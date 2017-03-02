af6340e3fafa34e581835adbe7fd3347b1c7a30f
Rafael Ordonez <rordonez@andigital.com>
2017-03-02 22:46:57 +0000

    Added a test to check the application context is loaded correctly

--- src/test/java/com/example/locations/LocationsApplicationTests.java
+++ src/test/java/com/example/locations/LocationsApplicationTests.java
@@ -1,16 +1,22 @@
 package com.example.locations;
 
+import com.example.locations.web.PostcodesController;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class LocationsApplicationTests {
+    @Autowired
+    private PostcodesController controller;
 
-	@Test
-	public void contextLoads() {
-	}
-
+    @Test
+    public void contextLoads() {
+        assertThat(controller).isNotNull();
+    }
 }
